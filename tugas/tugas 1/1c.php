6f190fd7d93533c151f2a0fd42fd31d2e2020bd8
tanwirul890 <tanrul890@gmail.com>
2025-02-22 12:23:09 +0700

    selesai mengerhakan tugas 1

---
+++ tugas/tugas 1/1c.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1c</title>
+
+    <style>
+        .container {
+            display: flex;
+        }
+
+        .kotak {
+            width: 30px;
+            height: 30px;
+            background-color: salmon;
+            border: 0.4px solid black;
+            text-align: center;
+            align-content: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="kotak">1</div>
+    </div>
+
+    <div class="container">
+        <div class="kotak">2</div>
+        <div class="kotak">2</div>
+    </div>
+
+    <div class="container">
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+    </div>
+    
+</body>
+</html>
